d81aeba4f38e63b6d84a999fa6b4f9eff44c875e
Liam Huber <liam.huber@gmail.com>
2021-04-28 16:07:48 +0200

    Add dummy tests

---
+++ tests/test_project.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+from pyiron_src._test import TestWithProject
+
+
+class TestProject(TestWithProject):
+
+    def test_void(self):
+        pass
